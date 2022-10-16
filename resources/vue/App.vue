a837552a18035995360f57be01ab91cf7ab4397b
MouadBNL <mouadbnl.2k01@gmail.com>
2022-10-16 13:24:36 +0100

    Vue 3 instalation

---
+++ resources/vue/App.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1>Hello world</h1>
+    <button @click="inc">{{ counter }}</button>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+
+const counter = ref(0)
+
+const inc = () => {
+    counter.value++
+}
+</script>
